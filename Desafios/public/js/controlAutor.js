8698eb6aa3da946215cf2d3676ed2fb4057e87b1
IgnacioBoccasile <ignacioboccasile@hotmail.com>
2017-02-12 14:33:55 -0300

    Login terminado, archivos residuales eliminados, cambios en diseño.

---
+++ Desafios/public/js/controlAutor.js
@@ -0,0 +1,9 @@
+angular.module('autor.controllers', [])
+
+.controller('AutorCtrl', function($scope, $timeout) {
+	 $scope.banderita = true;
+	
+	$timeout(function(){
+		 			$scope.banderita = false;
+		 		}, 3000);        
+});
